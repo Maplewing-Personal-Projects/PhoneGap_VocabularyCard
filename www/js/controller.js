1fde690107afc7f1d1630e1e3aadf27f558ed686
Maplewing <sinmaplewing@gmail.com>
2013-11-06 00:43:22 +0800

    Version 1.5 Update.

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,77 +1,208 @@
 $(function () {
-    var initData = {
-        Food: {
-            apple: "(n.) 蘋果",
-            banana: "(n.) 香蕉"
+
+    var initData = [
+        {
+            name: "Food",
+            vocabulary: [
+                {
+                    name: "apple",
+                    meaning: "(n.) 蘋果"
+                },
+                {
+                    name: "banana",
+                    meaning: "(n.) 香蕉"
+                }
+            ]
         },
-        Sport: {
-            jog: "(vi.) 慢跑"
+        {
+            name: "Sport",
+            vocabulary: [
+                {
+                    name: "jog",
+                    meaning: "(v.) 慢跑"
+                }
+            ]
         }
-    };
+    ];
 
     function nl2br(str) {
         return str.replace(/([^>])\n/g, '$1<br/>\n');
     }
 
+    var br2nl = function (varTest) {
+        return varTest.replace(/<br\/>/g, "\r");
+    };
+
     var Init = function () {
         var category;
-        if (window.localStorage && window.localStorage.categories) {
-            category = JSON.parse(window.localStorage.categories);
+        //window.localStorage.WZ_categories = JSON.stringify(initData);
+        if (window.localStorage && !window.localStorage.WZ_setting) {
+            WZ_setting = {
+                revert: false
+            };
+            window.localStorage.WZ_setting = JSON.stringify(WZ_setting);
+        }
+
+        if (window.localStorage && window.localStorage.WZ_categories ){
+            category = JSON.parse(window.localStorage.WZ_categories);
+        }
+        else if (window.localStorage && window.localStorage.categories) {
+            var tmp = JSON.parse(window.localStorage.categories);
+            category = [];
+            for (var i in tmp) {
+                var singleCategory = {
+                    name: i,
+                    vocabulary: []
+                };
+                for (var j in tmp[i]) {
+                    singleCategory.vocabulary.push({
+                        name: j,
+                        meaning: tmp[i][j]
+                    });
+                }
+                category.push(singleCategory);
+            }
+
+            window.localStorage.WZ_categories = JSON.stringify(category);
         }
         else if (window.localStorage) {
-            window.localStorage.categories = JSON.stringify(initData);
-            category = JSON.parse(window.localStorage.categories);
+            window.localStorage.WZ_categories = JSON.stringify(initData);
+            category = JSON.parse(window.localStorage.WZ_categories);
         }
         else {
             category = initData;
         }
-        for (var categoryName in category) {
-            $("#category-list").html($("#category-list").html() + "<li>" + categoryName + "</li>");
+
+        for (var i = 0 ; i < category.length ; ++i) {
+            $("#category-list").html($("#category-list").html() + "<li data-id='" + i + "'>" + category[i].name + "</li>");
         }
 
+        sortCategory();
     };
 
     var resetCategoryList = function () {
         $("#category-list").html('');
-        var category = JSON.parse(window.localStorage.categories);
-        for (var categoryName in category) {
-            $("#category-list").html($("#category-list").html() + "<li>" + categoryName + "</li>");
+        var category = JSON.parse(window.localStorage.WZ_categories);
+        for (var i = 0 ; i < category.length ; ++i) {
+            $("#category-list").html($("#category-list").html() + "<li data-id='" + i + "'>" + category[i].name + "</li>");
         }
+
+        sortCategory();
         clickCategoryEvent();
     }
+    
+    var sortCategory = function () {
+        $("#category-list").sortable({
+            stop: function () {
+                var category = JSON.parse(window.localStorage.WZ_categories);
+                var newCategory = [];
 
+                $('#category-list li').each(function () {
+                    newCategory.push(category[$(this).attr('data-id')]);
+                });
+                window.localStorage.WZ_categories = JSON.stringify(newCategory);
+                resetCategoryList();
+            }
+        }).disableSelection();
+    };
+    
     var resetVocabularyList = function () {
         $("#vocabulary-list").html('');
-        var category = JSON.parse(window.localStorage.categories);
-        var categoryName = $('#page-title2').html();
-        for (var vocabularyName in category[categoryName]) {
-            $("#vocabulary-list").html($("#vocabulary-list").html() + "<li>" + vocabularyName + "</li>");
+        var category = JSON.parse(window.localStorage.WZ_categories);
+        var categoryId = $('#page-title2').attr("data-id");
+        var revert = JSON.parse(window.localStorage.WZ_setting).revert;
+        for (var i = 0 ; i < category[categoryId].vocabulary.length; ++i) {
+            if (revert) {
+                $("#vocabulary-list").html($("#vocabulary-list").html() + "<li data-id='" + i + "'>" + category[categoryId].vocabulary[i].meaning + "</li>");
+            }
+            else {
+                $("#vocabulary-list").html($("#vocabulary-list").html() + "<li data-id='" + i + "'>" + category[categoryId].vocabulary[i].name + "</li>");
+            }
         }
+        sortVocabulary();
         clickVocabularyEvent();
     }
 
+    var sortVocabulary = function () {
+        $("#vocabulary-list").sortable({
+            stop: function () {
+                var category = JSON.parse(window.localStorage.WZ_categories);
+                var categoryId = $('#page-title2').attr("data-id");
+                var newVocabulary = [];
+
+                $('#vocabulary-list li').each(function () {
+                    newVocabulary.push(category[categoryId].vocabulary[$(this).attr('data-id')]);
+                });
+                
+                category[categoryId].vocabulary = newVocabulary;
+                window.localStorage.WZ_categories = JSON.stringify(category);
+                resetVocabularyList();
+            }
+        }).disableSelection();
+    };
+
+    var modifyCategory = function (value, dataId) {
+        if (window.localStorage.WZ_categories) {
+            var category = JSON.parse(window.localStorage.WZ_categories);
+            if (dataId !== "") {
+                category[dataId].name = value;
+            }
+            else {
+                category.push({ name: value, vocabulary: [] });
+            }
+            window.localStorage.WZ_categories = JSON.stringify(category);
+        }
+    };
+
+    var modifyVocabulary = function (value, meaning, categoryId, dataId) {
+        if (window.localStorage.WZ_categories) {
+            var category = JSON.parse(window.localStorage.WZ_categories);
+            if (dataId !== "") {
+                category[categoryId].vocabulary[dataId].name = value;
+                category[categoryId].vocabulary[dataId].meaning = meaning;
+            }
+            else {
+                category[categoryId].vocabulary.push({
+                    name: value,
+                    meaning: nl2br(meaning)
+                });
+            }
+            
+            window.localStorage.WZ_categories = JSON.stringify(category);
+        }
+    };
+
     $('#addCategoryForm').submit(function () {
         var value = $('#category').val();
-        if (window.localStorage.categories) {
-            var category = JSON.parse(window.localStorage.categories);
-            if (!category[value])
-                category[value] = {};
-            window.localStorage.categories = JSON.stringify(category);
-        }
+        var dataId = $('#category').attr("data-id");
+        modifyCategory(value, dataId);
 
         resetCategoryList();
-
-        $('#addCategory').slideUp('slow', function () {
-            $('#index').slideDown('slow');
+        allClose(function(){
+            if (dataId === "") $('#index').slideDown('slow');
+            else {
+                $('#page-title2').html(value);
+                $('#categoryContent').slideDown('slow');
+            }
         });
-
         return false;
     });
 
     var allClose = function (callback) {
         $('#about').slideUp('slow', function () {
             $('#addCategory').slideUp('slow', function () {
+                $('#click_index').html("返回分類列表");
+                $('#category').val("");
+                $('#category').attr("data-id", "");
+                $('#page-title1').html("新增分類");
+
                 $('#addVocabularyPage').slideUp('slow', function () {
+                    $('#click_category').html("返回分類");
+                    $('#vocabularyName').val("");
+                    $('#vocabularyName').attr("data-id", "");
+                    $('#vocabularyMeaning').val("");
+                    $('#page-title4').html("新增單字");
+
                     $('#vocabularyContent').slideUp('slow', function () {
                         $('#categoryContent').slideUp('slow', function () {
                             $('#index').slideUp('slow', function () {
@@ -85,9 +216,7 @@ $(function () {
     };
 
     $('#programName').click(function () {
-        allClose(function () {
-            $('#about').slideDown('slow');
-        });
+        $('#about').slideToggle('slow');
     });
 
     $('#click_addCategory').click(function () {
@@ -97,13 +226,49 @@ $(function () {
 
     });
 
-    $('#click_index, #click_index1, #click_index2').click(function () {
+    $('#click_editCategory').click(function () {
+        allClose(function () {
+            $('#click_index').html("返回分類");
+            $('#page-title1').html("編輯分類");
+            $('#category').val($("#page-title2").html());
+            $('#category').attr('data-id', $("#page-title2").attr('data-id'));
+            $('#addCategory').slideDown('slow');
+        });
+    });
+
+    $('#click_index').click(function () {
+        if ($('#category').attr("data-id") === "") {
+            allClose(function () {
+                $('#index').slideDown('slow');
+            });
+        }
+        else {
+            allClose(function () {
+                $('#categoryContent').slideDown('slow');
+            });
+        }
+    });
+
+    $('#click_index1').click(function () {
         allClose(function () {
             $('#index').slideDown('slow');
         });
     });
 
-    $('#click_category, #click_category1').click(function () {
+    $('#click_category').click(function () {
+        if ($('#vocabularyName').attr("data-id") === "") {
+            allClose(function () {
+                $('#categoryContent').slideDown('slow');
+            });
+        }
+        else {
+            allClose(function () {
+                $('#vocabularyContent').slideDown('slow');
+            });
+        }
+    });
+
+    $('#click_category1').click(function () {
         allClose(function () {
             $('#categoryContent').slideDown('slow');
         });
@@ -115,12 +280,24 @@ $(function () {
         });
     });
 
+    $('#click_editVocabulary').click(function () {
+        allClose(function () {
+            $('#click_category').html("返回單字");
+            $('#page-title4').html("編輯單字");
+            $('#vocabularyName').val($("#page-title3").html());
+            $('#vocabularyName').attr('data-id', $("#page-title3").attr('data-id'));
+            $('#vocabularyMeaning').val(br2nl($("#content").html()));
+            $('#addVocabularyPage').slideDown('slow');
+        });
+    });
+
+    
     $('#deleteCategory').click(function () {
         if (confirm('確定刪除此分類？')) {
-            var categoryName = $('#page-title2').html();
-            var category = JSON.parse(window.localStorage.categories);
-            delete category[categoryName];
-            window.localStorage.categories = JSON.stringify(category);
+            var categoryId = $('#page-title2').attr('data-id');
+            var category = JSON.parse(window.localStorage.WZ_categories);
+            category.splice( parseInt(categoryId), 1 );
+            window.localStorage.WZ_categories = JSON.stringify(category);
             resetCategoryList();
 
             allClose(function () {
@@ -132,10 +309,11 @@ $(function () {
     $('#deleteVocabulary').click(function () {
         if (confirm('確定刪除此單字？')) {
             var categoryName = $('#page-title2').html();
-            var category = JSON.parse(window.localStorage.categories);
-            var VocabularyName = $('#page-title3').html();
-            delete category[categoryName][VocabularyName];
-            window.localStorage.categories = JSON.stringify(category);
+            var categoryId = $('#page-title2').attr("data-id");
+            var category = JSON.parse(window.localStorage.WZ_categories);
+            var vocabularyId = $('#page-title3').attr("data-id");
+            category[categoryId].vocabulary.splice(parseInt(vocabularyId), 1);
+            window.localStorage.WZ_categories = JSON.stringify(category);
             resetVocabularyList();
 
             allClose(function () {
@@ -145,16 +323,21 @@ $(function () {
     });
 
     $('#addVocabularyForm').submit(function () {
-        if (confirm('確定新增此單字？')) {
+        var dataId = $('#vocabularyName').attr("data-id");
+        var promptString;
+
+        if (dataId === "") promptString = '確定新增此單字？';
+        else promptString = '確定修改此單字？'
+
+        if (confirm(promptString)) {
             var categoryName = $('#page-title2').html();
-            var category = JSON.parse(window.localStorage.categories);
+            var categoryId = $('#page-title2').attr("data-id");
+            
             var vocabularyName = $('#vocabularyName').val();
             var vocabularyMeaning = $('#vocabularyMeaning').val();
-            var vocabulary = category[categoryName][vocabularyName];
-            category[categoryName][vocabularyName] = nl2br(vocabularyMeaning);
-
-            window.localStorage.categories = JSON.stringify(category);
-
+            
+            
+            modifyVocabulary(vocabularyName, vocabularyMeaning, categoryId, dataId);
             resetVocabularyList();
 
             allClose(function () {
@@ -164,21 +347,37 @@ $(function () {
         return false;
     });
 
-    Init();
+    $('#revertContent').click(function () {
+        WZ_setting = JSON.parse(window.localStorage.WZ_setting)
+        WZ_setting.revert = !WZ_setting.revert;
+        window.localStorage.WZ_setting = JSON.stringify(WZ_setting);
 
+        resetVocabularyList();
+
+    });
 
     var clickCategoryEvent = function () {
         $('#category-list li').on('click', function () {
             var categoryName = $(this).html();
-            var category = JSON.parse(window.localStorage.categories);
+            var categoryId = $(this).attr("data-id");
+            var category = JSON.parse(window.localStorage.WZ_categories);
             $('#page-title2').html(categoryName);
-            var vocabulary = category[categoryName];
+            $('#page-title2').attr("data-id", categoryId);
+            var vocabulary = category[categoryId].vocabulary;
+
             $('#vocabulary-list').html('');
-            for (var vocabularyName in vocabulary) {
-                $('#vocabulary-list').html($('#vocabulary-list').html() + '<li>' + vocabularyName + '</li>');
+            var revert = JSON.parse(window.localStorage.WZ_setting).revert;
+            for (var i = 0 ; i < category[categoryId].vocabulary.length; ++i) {
+                if (revert) {
+                    $("#vocabulary-list").html($("#vocabulary-list").html() + "<li data-id='" + i + "'>" + category[categoryId].vocabulary[i].meaning + "</li>");
+                }
+                else {
+                    $("#vocabulary-list").html($("#vocabulary-list").html() + "<li data-id='" + i + "'>" + category[categoryId].vocabulary[i].name + "</li>");
+                }
             }
 
             clickVocabularyEvent();
+            sortVocabulary();
 
             allClose(function () {
                 $('#categoryContent').slideDown('slow');
@@ -189,12 +388,15 @@ $(function () {
     var clickVocabularyEvent = function () {
         $('#vocabulary-list li').on("click", function () {
             var categoryName = $('#page-title2').html();
-            var vocabularyName = $(this).html();
-            var category = JSON.parse(window.localStorage.categories);
-            var vocabulary = category[categoryName][vocabularyName];
-
+            var categoryId = $('#page-title2').attr("data-id");
+            
+            var vocabularyId = $(this).attr("data-id");
+            var category = JSON.parse(window.localStorage.WZ_categories);
+            var vocabulary = category[categoryId].vocabulary[vocabularyId].meaning;
+            var vocabularyName = category[categoryId].vocabulary[vocabularyId].name;
 
             $('#page-title3').html(vocabularyName);
+            $('#page-title3').attr("data-id", vocabularyId);
             $('#content').html(vocabulary);
 
             allClose(function () {
@@ -203,5 +405,6 @@ $(function () {
         });
     }
 
+    Init();
     clickCategoryEvent();
 });
